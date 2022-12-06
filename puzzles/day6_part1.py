4df7419326a7db1d8b2aee76cfd2fd4c8d8ea650
Darien Pérez <dapec_290@hotmail.com>
2022-12-06 13:08:24 -0600

    Day 6 part 1 done

---
+++ puzzles/day6_part1.py
@@ -0,0 +1,20 @@
+stream = input()
+
+for c_index in range(len(stream) - 3):
+    new_charset = ''
+    is_start_of_packet = True
+
+    for i in range(4):
+        my_char = stream[c_index + i]
+        if my_char not in new_charset:
+            new_charset += my_char
+        else:
+            is_start_of_packet = False
+            break
+
+    if is_start_of_packet:
+        start_of_packet_marker_pos = c_index + 4
+        break
+
+
+print(start_of_packet_marker_pos)
