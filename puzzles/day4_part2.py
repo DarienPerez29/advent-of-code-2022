0dfaf868e58277f1847347ca7b89e8de16033432
Darien Pérez <dapec_290@hotmail.com>
2022-12-06 12:06:33 -0600

    Day 4 part 2 done

---
+++ puzzles/day4_part2.py
@@ -0,0 +1,21 @@
+pairs = 0
+
+while True:
+    line = input()
+
+    if line == "eof":
+        break
+
+    elf1, elf2 = line.split(",")
+
+    elf1_lower, elf1_higher = elf1.split("-")
+    elf2_lower, elf2_higher = elf2.split("-")
+
+    elf1_lower, elf1_higher = int(elf1_lower), int(elf1_higher)
+    elf2_lower, elf2_higher = int(elf2_lower), int(elf2_higher)
+
+    if elf1_higher >= elf2_lower and elf2_higher >= elf1_lower:
+        print("Overlaps")
+        pairs += 1
+
+print(pairs)
